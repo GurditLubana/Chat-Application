0931cc06903f6f1f784d740272496b5734db9572
Gurdit Singh <105954455+GurditLubana@users.noreply.github.com>
2024-03-19 09:39:34 -0500

    Removed warnings

--- front-end/src/components/MessageBody.jsx
+++ front-end/src/components/MessageBody.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { React, useContext, useEffect, useRef, useState } from "react";
 import Container from "react-bootstrap/Container";
 import "./MessageBody.css";
